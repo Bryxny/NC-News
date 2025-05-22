17341242dbce3436aa715f0d32b22811711aa12c
Bryxny <bryonydoh2@gmail.com>
2025-05-22 10:55:16 +0100

    cleaned up voting

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -3,55 +3,41 @@ import { useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { useContext } from "react";
 export default function Voting({ article }) {
-  const [hasVoted, setHasVoted] = useState(false);
   const [votes, setVotes] = useState(article.votes);
-  const [voteValue, setVoteValue] = useState(0);
+  const [currentVote, setCurrentVote] = useState(0);
   const { user } = useContext(UserContext);
 
-  function handleVote(vote) {
-    if (!hasVoted) {
-      setHasVoted(true);
-      patchArticleVote(article.article_id, vote);
-      setVotes(votes + vote);
-      setVoteValue(vote);
-    } else if (vote === voteValue) {
-      setHasVoted(false);
-      patchArticleVote(article.article_id, -vote);
-      setVotes(votes - vote);
-      setVoteValue(0);
-    } else {
-      setVotes(votes + 2 * vote);
-      setVoteValue(vote);
-      patchArticleVote(article.article_id, 2 * vote);
-    }
+  function handleVote(voteToAdd) {
+    const isSameVote = voteToAdd === currentVote;
+    const voteChange = isSameVote ? -currentVote : voteToAdd - currentVote;
+
+    setCurrentVote(isSameVote ? 0 : voteToAdd);
+    setVotes(votes + voteChange);
+    patchArticleVote(article.article_id, voteChange);
   }
+
+  if (!user) return <p>{votes} votes - sign in to vote</p>;
   return (
     <>
       <div className="votes-box">
         <p>{votes} votes</p>
-        {!user ? (
-          <p>sign in to vote</p>
-        ) : (
-          <div className="votes-box">
-            <button
-              className={voteValue === 1 ? "clicked" : "unclicked"}
-              onClick={() => {
-                handleVote(1);
-              }}
-            >
-              upvote
-            </button>
-            <button
-              className={voteValue === -1 ? "clicked" : "unclicked"}
-              onClick={() => {
-                handleVote(-1);
-              }}
-            >
-              downvote
-            </button>
-            {hasVoted && <p className="voted">voted!</p>}
-          </div>
-        )}
+        <button
+          className={currentVote === 1 ? "clicked" : "unclicked"}
+          onClick={() => {
+            handleVote(1);
+          }}
+        >
+          upvote
+        </button>
+        <button
+          className={currentVote === -1 ? "clicked" : "unclicked"}
+          onClick={() => {
+            handleVote(-1);
+          }}
+        >
+          downvote
+        </button>
+        {currentVote ? <p className="voted">voted!</p> : null}
       </div>
     </>
   );
